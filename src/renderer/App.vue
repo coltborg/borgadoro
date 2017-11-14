177459a2e8e1b2197a2ae9cdff5fccebb479c021
legendofcolt <coltborg@gmail.com>
2017-11-14 16:44:27 -0600

    Add basic styles to elements
    Import tachyons from CDN
    
    Add base colors to root
    
    Add classes outside of tachyons with `_` prefix
    
    Add grid layout to landing page
    
    Add active change to play button
    
    Closes #2

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="_black _bg-white">
     <router-view></router-view>
   </div>
 </template>
@@ -11,5 +11,72 @@
 </script>
 
 <style>
-  /* CSS */
+@import url('https://fonts.googleapis.com/css?family=Nunito:300,400,600');
+@import url('https://unpkg.com/tachyons@4.9.0/css/tachyons.min.css');
+
+:root {
+  --black: #253137;
+  --gray: #8f9698;
+  --white: #ffffff;
+  --blue: #0088ff;
+  --purple: #7349ff;
+  --yellow: #ffc524;
+  --peach: #ffc4A8;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  /* !!! Remove this soon */
+  outline: none;
+}
+
+body {
+  font-family: 'Nunito', sans-serif;
+  font-size: 100%;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+
+.vw-100 { width: 100vw; }
+
+._black { color: var(--black); }
+._gray  { color: var(--gray); }
+._white { color: var(--white); }
+._blue  { color: var(--blue); }
+
+._bg-black { background-color: var(--black); }
+._bg-white { background-color: var(--white); }
+._bg-gray  { background-color: var(--gray); }
+._bg-blue  { background-color: var(--blue); }
+
+._hover-bg-black:hover { background-color: var(--black); }
+._hover-bg-white:hover { background-color: var(--white); }
+._hover-bg-gray:hover  { background-color: var(--gray); }
+._hover-bg-blue:hover  { background-color: var(--blue); }
+._hover-bg-purple:hover  { background-color: var(--purple); }
+._hover-bg-yellow:hover  { background-color: var(--yellow); }
+._hover-bg-peach:hover  { background-color: var(--peach); }
+
+._bg-animate { transition: background-color 150ms ease-out; }
+._color-animate { transition: color 150ms ease-out; }
+._all-animate { transition: all 150ms ease-out; }
+/* transition: all 150ms cubic-bezier(0.25, 0.8, 0.25, 1); */
+
+._gray-shadow { box-shadow: 0 0.25rem 1rem 0.25rem rgba(143, 150, 152, 0.25); }
+._blue-shadow { box-shadow: 0 0.25rem 1rem 0.25rem rgba(0, 136, 255, 0.25); }
+
+.fade-enter-active {
+  transition: all 150ms ease-out;
+}
+.fade-leave-active {
+  transition: all 150ms ease-out;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,10 +1,73 @@
 <template>
-<div class="grid">
-  <p>{{ count }} Pomodoro Today</p>
-  <h1>25:00</h1>
-  <div>
-    <button>Start</button>
-    <button>Reset</button>
+<div class="grid vh-100 vw-100 tc">
+  <p class="mb2 area-meta _gray f4 fw6 lh-copy">{{ count }} Pomodoro Today</p>
+  <h1 class="mt0 mb4 area-time f-headline fw2 lh-title">25:00</h1>
+  <div class="area-actions">
+    <button
+      type="button"
+      class="mr3 f4 fw2 bn br2 ph3 pv2 pointer inline-flex items-center _gray-shadow _gray _bg-white _all-animate"
+      :class="activeStyles"
+      @click="handlePlay">
+      <transition name="fade" mode="out-in">
+        <svg
+          v-if="!isActive"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          aria-labelledby="title"
+          aria-describedby="desc"
+          role="img"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="16"
+          height="16"
+          style="width: 16px; height: 16px;"
+          class="mr2">
+          <title>Play</title>
+          <desc>A solid styled icon from Orion Icon Library.</desc>
+          <path data-name="layer1"
+          fill="currentColor" d="M6 2l52 30L6 62V2z"></path>
+        </svg>
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          aria-labelledby="title"
+          aria-describedby="desc"
+          role="img"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="16"
+          height="16"
+          style="width: 16px; height: 16px;"
+          class="mr2">
+          <title>Pause</title>
+          <desc>A solid styled icon from Orion Icon Library.</desc>
+          <path data-name="layer1" fill="currentColor" d="M40 4h12v56H40z"></path>
+          <path data-name="layer2" fill="currentColor" d="M12 4h12v56H12z"></path>
+        </svg>
+      </transition>
+      Start
+    </button>
+    <button
+      type="button"
+      class="f4 fw2 bn br2 ph3 pv2 pointer inline-flex items-center _gray-shadow _gray _bg-white">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 64 64"
+        aria-labelledby="title"
+        aria-describedby="desc"
+        role="img"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        width="16"
+        height="16"
+        style=""
+        class="mr2">
+        <title>Rotate Right</title>
+        <desc>A solid styled icon from Orion Icon Library.</desc>
+        <path data-name="layer1"
+        d="M39.777 28.593l12.26 11.273L62 26l-4.63-3.103-3.332 4.11A26.995 26.995 0 0 0 28 6C13.48 6 2 18.488 2 33a25.791 25.791 0 0 0 26 26 26.924 26.924 0 0 0 18.953-7.719l-4.244-4.241A20.967 20.967 0 0 1 28 53 19.786 19.786 0 0 1 8 33c0-11.203 8.788-21 20-21a20.979 20.979 0 0 1 20.126 16.12l-4.288-3.943z"
+        fill="currentColor"></path>
+      </svg>
+      Reset
+    </button>
   </div>
 
 </div>
@@ -16,28 +79,43 @@ export default {
   data() {
     return {
       count: 0,
+      isActive: false,
     };
+  },
+  computed: {
+    activeStyles() {
+      return {
+        '_bg-blue': this.isActive,
+        _white: this.isActive,
+        '_blue-shadow': this.isActive,
+      };
+    },
   },
   methods: {
     open(link) {
       this.$electron.shell.openExternal(link);
+    },
+    toggleActive() {
+      this.isActive = !this.isActive;
+    },
+    handlePlay() {
+      this.toggleActive();
     },
   },
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Nunito:300,400,600');
-
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+.grid {
+  display: grid;
+  grid-template-areas: "meta" "time" "actions";
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(3, auto);
+  justify-content: center;
+  align-content: center;
 }
 
-body {
-  font-family: 'Nunito', sans-serif;
-}
-
-
+.area-meta { grid-area: meta; }
+.area-time { grid-area: time; }
+.area-actions { grid-area: actions; }
 </style>
